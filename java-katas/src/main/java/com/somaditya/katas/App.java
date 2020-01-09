ede69cdc74504f0047957a511c70d43002f95624
Somaditya Basak <mail@somaditya.com>
2020-01-09 20:08:39 +0530

    added maven archetype

---
+++ java-katas/src/main/java/com/somaditya/katas/App.java
@@ -0,0 +1,13 @@
+package com.somaditya.katas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
