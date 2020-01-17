b201bcdbd3087ba68198ad1b55fa44e2f1968951
Somaditya Basak <mail@somaditya.com>
2020-01-18 01:28:32 +0530

    Char freq counter with hashmap

---
+++ java/src/main/java/com/somaditya/katas/ds/CharFreq.java
@@ -0,0 +1,18 @@
+package com.somaditya.katas.ds;
+
+import java.util.HashMap;
+
+public class CharFreq {
+    public HashMap<Character, Integer> count(String input) {
+        HashMap<Character, Integer> hm = new HashMap<>();
+    
+        for (Character c : input.toCharArray()) {
+            if (hm.containsKey(c)) {
+                hm.put(c, hm.get(c) + 1);
+            } else {
+                hm.put((c), 1);
+            }
+        }
+        return hm;
+    }
+}
