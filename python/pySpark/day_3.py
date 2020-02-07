23826601d4e6d0a61b2c1c38c7d6fe5ab8e98d86
somaditya <somaditya.b@gmail.com>
2020-02-07 11:44:48 +0530

    day 2,3 def main

--- python/pySpark/day_3.py
+++ python/pySpark/day_3.py
@@ -1,105 +1,111 @@
 import math
 import calendar
 
-floor = math.floor(-3.6)
-print(floor)
 
-round = abs(round(-5.6))
-print(round)
+def main():
+    floor = math.floor(-3.6)
+    print(floor)
 
-leap_count = 0
+    rounded = abs(round(-5.6))
+    print(rounded)
 
-for year in (range(2000, 2050)):
-    if (calendar.isleap(year)):
-        leap_count += 1
+    leap_count = 0
 
-print("leap years:", leap_count)
+    for year in (range(2000, 2050)):
+        if (calendar.isleap(year)):
+            leap_count += 1
 
-day_of_week = calendar.weekday(2018, 9, 26)
-print(day_of_week)
+    print("leap years:", leap_count)
 
-season = "summer"
+    day_of_week = calendar.weekday(2018, 9, 26)
+    print(day_of_week)
 
-new_season = "Ilove" + season + "!"
-print(new_season)
+    season = "summer"
 
-side1 = 4
-side2 = 5
-side3 = 6
+    new_season = "Ilove" + season + "!"
+    print(new_season)
 
-print("The sides have lengths", side1, ",", side2, ",", side3, ".")
+    side1 = 4
+    side2 = 5
+    side3 = 6
 
-ids = [5343, 1234, 5692, 3245, 9764, 2538]
+    print("The sides have lengths", side1, ",", side2, ",", side3, ".")
 
-ids.remove(3245)
-print(ids)
+    ids = [5343, 1234, 5692, 3245, 9764, 2538]
 
-print(ids.index(9764))
+    ids.remove(3245)
+    print(ids)
 
-ids.insert(4, 5588)
-print(ids)
+    print(ids.index(9764))
 
-ids.extend([4363, 2368])
-print(ids)
+    ids.insert(4, 5588)
+    print(ids)
 
-ids.reverse()
-print(ids)
+    ids.extend([4363, 2368])
+    print(ids)
 
-ids.sort()
-print(ids)
+    ids.reverse()
+    print(ids)
 
-temps = [22.5, 17.8, 30.4, 26.9, 27, 23.5, 18.6]
+    ids.sort()
+    print(ids)
 
-temps.sort()
-print(temps)
+    temps = [22.5, 17.8, 30.4, 26.9, 27, 23.5, 18.6]
 
-cool_temps = temps[:2]
-print(cool_temps)
+    temps.sort()
+    print(temps)
 
-warm_temps = temps[3:]
-print(warm_temps)
+    cool_temps = temps[:2]
+    print(cool_temps)
 
-temps_in_celsius = cool_temps + warm_temps
-print(temps_in_celsius)
+    warm_temps = temps[3:]
+    print(warm_temps)
 
-units = [['km', 'miles', 'league'], ['kg', 'pound', 'stone']]
+    temps_in_celsius = cool_temps + warm_temps
+    print(temps_in_celsius)
 
-print(units[0])
-print(units[1])
-print(units[0][0])
-print(units[1][0])
-print(units[0][1:])
-print(units[1][0:2])
+    units = [['km', 'miles', 'league'], ['kg', 'pound', 'stone']]
 
-print("Alkaline Earth Metals")
-print("+++++++++++++++++++++")
-alkaline_earth_metals = [[4, 9.012], [12, 24.305], [20, 40.078], [38, 87.63], [56, 137.327], [88, 226]]
+    print(units[0])
+    print(units[1])
+    print(units[0][0])
+    print(units[1][0])
+    print(units[0][1:])
+    print(units[1][0:2])
 
-for metal in alkaline_earth_metals:
-    print(metal)
+    print("Alkaline Earth Metals")
+    print("+++++++++++++++++++++")
+    alkaline_earth_metals = [[4, 9.012], [12, 24.305], [20, 40.078], [38, 87.63], [56, 137.327], [88, 226]]
 
-number_and_weight = []
+    for metal in alkaline_earth_metals:
+        print(metal)
 
-for metal in alkaline_earth_metals:
-    for item in metal:
-        number_and_weight.append(item)
+    number_and_weight = []
 
-print(number_and_weight)
+    for metal in alkaline_earth_metals:
+        for item in metal:
+            number_and_weight.append(item)
 
-cat_1 = [4, 4, 4, 4, 5, 5, 5, 5, 4, 7]
-cat_2 = [5, 5, 5, 5, 5, 6, 6, 6, 6, 6]
+    print(number_and_weight)
 
-if cat_1[0] > cat_2[0]:
-    print("Cat 1 weighed more than cat 2 on day 1.")
-elif cat_1[0] < cat_2[0]:
-    print("Cat 1 weighed less than cat 2 on day 1.")
-else:
-    print("cat1 == cat2")
+    cat_1 = [4, 4, 4, 4, 5, 5, 5, 5, 4, 7]
+    cat_2 = [5, 5, 5, 5, 5, 6, 6, 6, 6, 6]
 
-if cat_1[0] > cat_2[0] and cat_1[9] > cat_2[9]:
-    print("Cat 1 remained heavier than cat 2.")
-else:
-    print("Cat 2 became heavier than Cat 1.")
+    if cat_1[0] > cat_2[0]:
+        print("Cat 1 weighed more than cat 2 on day 1.")
+    elif cat_1[0] < cat_2[0]:
+        print("Cat 1 weighed less than cat 2 on day 1.")
+    else:
+        print("cat1 == cat2")
 
-for n in range(34, 52):
-    print(n, end=" ")
+    if cat_1[0] > cat_2[0] and cat_1[9] > cat_2[9]:
+        print("Cat 1 remained heavier than cat 2.")
+    else:
+        print("Cat 2 became heavier than Cat 1.")
+
+    for n in range(34, 52):
+        print(n, end=" ")
+
+
+if __name__ == "__main__":
+    main()
