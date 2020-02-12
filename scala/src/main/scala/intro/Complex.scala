c81a556bbadf4deaa2e3663ad99839634fca8156
somaditya <somaditya.b@gmail.com>
2020-02-12 14:51:06 +0530

    complex num

--- scala/src/main/scala/intro/Complex.scala
+++ scala/src/main/scala/intro/Complex.scala
@@ -3,6 +3,16 @@ package intro
 class Complex(real: Double, imaginary: Double) {
   def re = real
   def im = imaginary
+
+  override def toString: String = re.toString + (if (im > 0) s" + $im" + "i" else "")
+}
+
+object ComplexTest {
+  def main(args: Array[String]): Unit = {
+    val c = new Complex(4,0.5)
+
+    println(c.toString)
+  }
 }
 
 
