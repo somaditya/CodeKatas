4e642a95bd3a389b4cd40460ae48e4180ff4dd27
Somaditya Basak <mail@somaditya.com>
2020-04-20 00:00:16 +0530

    add PrimalityTest

---
+++ java/src/main/java/com/somaditya/katas/strings/TagContentExtractor.java
@@ -0,0 +1,23 @@
+package com.somaditya.katas.strings;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class TagContentExtractor {
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while (testCases > 0) {
+            String line = in.nextLine();
+            
+            String content = line.substring(line.indexOf(">") + 1, line.indexOf("<", line.indexOf(">")));
+            System.out.println(content);
+            
+            testCases--;
+        }
+    }
+}
